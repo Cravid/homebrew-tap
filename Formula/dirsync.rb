5a82402293f043cd12a1b10bbc85a21e55d40b95
David Schunke <cravid@schunke.me>
2020-12-03 22:45:07 +0100

    updated version of dirsync

--- Formula/dirsync.rb
+++ Formula/dirsync.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Dirsync < Formula
   desc ""
   homepage ""
-  url "https://github.com/Cravid/dirsync/archive/v1.0.0.tar.gz"
-  sha256 "b2c91fd132b4b6e6cc861051c5336f6e37b7881893d9cf4d98de01fcf0daed37"
+  url "https://github.com/Cravid/dirsync/archive/v1.0.1.tar.gz"
+  sha256 "8ec7a50d53b017b89600017a04fca97f762c25440a86d867cb6dd185f3977884"
   license ""
 
   # depends_on "cmake" => :build
