f75a9024f9c73c6e2ee1446ceed80be8506f1e0a
David Schunke <cravid@schunke.me>
2020-12-05 20:43:22 +0100

    added humanise formula

---
+++ Formula/humanise.rb
@@ -0,0 +1,14 @@
+class Humanise < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Cravid/humanise/", :using => :git, :tag => "v1.0.0"
+  sha256 "d9ecbf902fa42b2584d0606acb4dc341cc649089a55ff085e092b7aec0987267"
+  license ""
+
+  def install
+    bin.install "humanise"
+  end
+
+  test do
+  end
+end
