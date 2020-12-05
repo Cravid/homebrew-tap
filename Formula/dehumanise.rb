4aa267a0a373a4b5afc814ced0807ac3db474959
David Schunke <cravid@schunke.me>
2020-12-05 17:19:15 +0100

    added dehumanise formula

---
+++ Formula/dehumanise.rb
@@ -0,0 +1,14 @@
+class Dirsync < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Cravid/dehumanise/archive/v1.0.0.tar.gz"
+  sha256 "cd439ae8bcd4509e8e8582bbbd71afd7319a2309e8dba33f356f28fc543e241c"
+  license ""
+
+  def install
+    bin.install "dehumanise"
+  end
+
+  test do
+  end
+end
