3afc2921be095e3fa649c644a1a64ae27549dff0
David Schunke <cravid@schunke.me>
2020-12-05 20:46:13 +0100

    changed humanise to not rely on git

--- Formula/humanise.rb
+++ Formula/humanise.rb
@@ -1,7 +1,7 @@
 class Humanise < Formula
   desc ""
   homepage ""
-  url "https://github.com/Cravid/humanise/", :using => :git, :tag => "v1.0.0"
+  url "https://github.com/Cravid/humanise/archive/v1.0.0.tar.gz"
   sha256 "d9ecbf902fa42b2584d0606acb4dc341cc649089a55ff085e092b7aec0987267"
   license ""
 
