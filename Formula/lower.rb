e8a0b44fbf9adcfeea344d28c1406a5f86a5c466
David Schunke <cravid@schunke.me>
2020-12-14 13:33:09 +0100

    added new lower formula

---
+++ Formula/lower.rb
@@ -0,0 +1,15 @@
+class Lower < Formula
+  desc ""
+  homepage ""
+  
+  url "https://github.com/Cravid/lower/archive/v1.0.0.tar.gz"
+  sha256 "69c2a3fa8f4391a96ed5528b70649f50850f396e3057cfeef750057ff707a741"
+  license ""
+
+  def install
+    bin.install "lower"
+  end
+
+  test do
+  end
+end
