b9f15e6763ea51635a659cdc8bd5500440772398
David Schunke <cravid@schunke.me>
2020-12-14 18:12:11 +0100

    added upper formula

---
+++ Formula/upper.rb
@@ -0,0 +1,15 @@
+class Upper < Formula
+  desc ""
+  homepage ""
+  
+  url "https://github.com/Cravid/upper/archive/v1.0.0.tar.gz"
+  sha256 "2e6a2b1e61ae8f46ed74a73e825a55bc24e6945cb5da116d2cefe56e576e29ab"
+  license ""
+
+  def install
+    bin.install "upper"
+  end
+
+  test do
+  end
+end
