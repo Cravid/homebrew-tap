1f10824a2a38e7bbc87d1153b05a8e9d90dabbce
David Schunke <cravid@schunke.me>
2020-12-14 17:53:13 +0100

    changed version of fsearch to v1.0.2

--- Formula/fsearch.rb
+++ Formula/fsearch.rb
@@ -1,13 +1,13 @@
 class Fsearch < Formula
   desc ""
   homepage ""
-  revision 2
   
-  url "https://github.com/Cravid/fsearch/archive/v1.0.1.tar.gz"
-  sha256 "f111e1147296117eab3581bc461c04193ef1059cc588c13b9c2e0dcfda01f8e1"
+  url "https://github.com/Cravid/fsearch/archive/v1.0.2.tar.gz"
+  sha256 "286be1de7ae6c61b1e0be0d0c51f6857077099606908690f65181dab60fd7e15"
   license ""
 
   depends_on "cravid/tap/dehumanise"
+  depends_on "cravid/tap/lower"
   
   def install
     bin.install "fsearch"
