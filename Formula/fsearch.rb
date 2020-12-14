e67700caea89d887051fbb7a40ba6e3009569ea2
David Schunke <cravid@schunke.me>
2020-12-14 17:47:20 +0100

    changed fsearch to revision 2 for update

--- Formula/fsearch.rb
+++ Formula/fsearch.rb
@@ -1,7 +1,7 @@
 class Fsearch < Formula
   desc ""
   homepage ""
-  revision 1
+  revision 2
   
   url "https://github.com/Cravid/fsearch/archive/v1.0.1.tar.gz"
   sha256 "f111e1147296117eab3581bc461c04193ef1059cc588c13b9c2e0dcfda01f8e1"
