ea71c12ac6cb17009730f1a544e726e6a31b9107
David Schunke <cravid@schunke.me>
2021-01-06 15:38:23 +0100

    fixed scp-bench naming

--- Formula/scp-bench.rb
+++ Formula/scp-bench.rb
@@ -1,4 +1,4 @@
-class Scp-bench < Formula
+class ScpBench < Formula
   homepage ""
   url "https://github.com/Cravid/scp-bench/archive/v1.0.0.zip"
   sha256 "8ed74458c2a8f97a56dca6c9b4651761e41a717b9e3325bc17c2ed48e0bdeab9"
