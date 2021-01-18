277769531b12726d968c7d0d7b19c10a6b39df9b
David Schunke <cravid@schunke.me>
2021-01-18 14:05:35 +0100

    added prompt formula

---
+++ Formula/prompt.rb
@@ -0,0 +1,12 @@
+class Prompt < Formula
+    desc ""
+    homepage "https://github.com/Cravid/prompt/"
+    url "https://github.com/Cravid/prompt/archive/v1.0.0.tar.gz"
+    sha256 "0eb46f4e04e191f194952b4ed64aec5a0ccc03cb7f572401d7be09d1bb1aa277"
+    license "MIT"
+
+    def install
+      bin.install "prompt"
+    end
+  end
+  
