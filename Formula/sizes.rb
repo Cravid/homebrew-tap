6474608078019cbadcd3ba73c468f2211f85ad39
David Schunke <cravid@schunke.me>
2021-02-07 13:07:48 +0100

    updated sizes from v1.1.0 to v1.1.1

--- Formula/sizes.rb
+++ Formula/sizes.rb
@@ -1,7 +1,7 @@
 class Sizes < Formula
   homepage "https://github.com/Cravid/sizes/"
-  url "https://github.com/Cravid/sizes/archive/v1.1.0.tar.gz"
-  sha256 "8d50126b77971c8d40d5066537bc3332434971dc0308d366185819f1cfb96c4c"
+  url "https://github.com/Cravid/sizes/archive/v1.1.1.tar.gz"
+  sha256 "a8bbd1152590e11ccdb5617e018759fd7bc03b4d6dbf93672ab246359ccf24e3"
   license "MIT"
 
   def install
