68d66ad90a966e00372261cf59e0771466668aec
David Schunke <cravid@schunke.me>
2021-03-24 13:07:17 +0100

    updated bench from v1.0.0 to v1.0.1

--- Formula/bench.rb
+++ Formula/bench.rb
@@ -1,7 +1,7 @@
 class Bench < Formula
   homepage "https://github.com/Cravid/bench/"
-  url "https://github.com/Cravid/bench/archive/v1.0.0.zip"
-  sha256 "5fde1805f25b97e220e3811e12a78197bae2f5b19e0c564c7bdbaf08b5d9ef33"
+  url "https://github.com/Cravid/bench/archive/v1.0.1.zip"
+  sha256 "8dcc32615547b2ddd9f7bb5484dacd27a8fcab0893ee7cb0c34b344c9ba72ad5"
   license "MIT"
 
   depends_on "Cravid/tap/dehumanise"
