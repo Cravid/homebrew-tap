dea3813824d32c16ebdffe6f73e2476f7745737f
David Schunke <cravid@schunke.me>
2021-04-20 14:26:35 +0200

    updated dirsync hash

--- Formula/dirsync.rb
+++ Formula/dirsync.rb
@@ -1,7 +1,7 @@
 class Dirsync < Formula
   homepage "https://github.com/Cravid/dirsync"
   url "https://github.com/Cravid/dirsync/archive/v1.1.0.tar.gz"
-  sha256 "b2c91fd132b4b6e6cc861051c5336f6e37b7881893d9cf4d98de01fcf0daed37"
+  sha256 "6d232bf0e498d70679e56a98ae3391a628c3ebec6cbfc4ed53593e779e703c5e"
   license "MIT"
 
   # depends_on "cmake" => :build
